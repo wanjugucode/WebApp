4995f3184ca187573d5fb296929b73d764c9faa4
wanjugucode <karobiaanastasia.com>
2021-11-10 16:19:48 +0300

    add requirements.txt

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -91,9 +91,17 @@ WSGI_APPLICATION = 'inventory_project.wsgi.application'
 
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'expresskitchendb;',
+        'USER': 'expresskitchen',
+        'PASSWORD': 'AkiraChix2021',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
